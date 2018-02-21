691b3381aa49b4e128315d38a9b1ed9a4a184b67
David Warburton <david.warburton@gmail.com>
2018-02-21 11:43:24 -0800

    implement

--- main.cpp
+++ main.cpp
@@ -3,6 +3,37 @@
 using namespace std;
 
 int main() {
-    cout << "hello world." << endl;
-    return 0;
+    std::string numerals = "XVII";
+    int val = 0;
+    int sum = 0;
+    int prev = 0;
+    for(char& c : numerals) {
+        switch (c) {
+            case 'I':
+                val = 1;
+                break;
+            case 'V': 
+                val = 5;
+                break;
+            case 'X':
+                val = 10;
+                break;
+            case 'L':
+                val = 50;
+                break;
+        }
+        if (prev != 0) {
+            if (prev > val) {
+                sum += prev;
+            } else if (prev == val) {
+                sum +-= prev;
+            } else {
+                sum -= prev;
+            }
+        }
+        prev = val;
+    }
+    sum += prev;
+    cout << "Result: " << sum  << endl;
+    return sum;
 }
